4697babebf5578f334e08404dd4d07f699043b31
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-09-04 16:49:18 +0900

    style: split interface file

---
+++ src/MagicOnionLab.Shared/Hubs/IGameHubReceiver.cs
@@ -0,0 +1,32 @@
+﻿using MagicOnionLab.Shared.Mpos;
+
+namespace MagicOnionLab.Shared.Hubs
+{
+    public interface IGameHubReceiver
+    {
+        /// <summary>
+        /// Notify when Created room
+        /// </summary>
+        /// <param name="roomName"></param>
+        void OnCreateRoom(string roomName);
+        /// <summary>
+        /// Notify when Joined room
+        /// </summary>
+        /// <param name="userName"></param>
+        void OnJoinRoom(string userName);
+        /// <summary>
+        /// Notify when leaved room
+        /// </summary>
+        /// <param name="userName"></param>
+        void OnLeaveRoom(string userName);
+        /// <summary>
+        /// Notify when match complated
+        /// </summary>
+        void OnMatchCompleted();
+        /// <summary>
+        /// Notify when updated userinfo
+        /// </summary>
+        /// <param name="response"></param>
+        void OnUpdateUserInfo(GameRoomUserInfoUpdateResponse response);
+    }
+}
