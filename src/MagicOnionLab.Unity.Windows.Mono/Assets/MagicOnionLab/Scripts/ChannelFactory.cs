d1205d4a82ff88ba169939b075e69b70460066ad
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-09-06 20:04:09 +0900

    feat: reuse channel if possible

--- src/MagicOnionLab.Unity.Windows.Mono/Assets/MagicOnionLab/Scripts/ChannelFactory.cs
+++ src/MagicOnionLab.Unity.Windows.Mono/Assets/MagicOnionLab/Scripts/ChannelFactory.cs
@@ -1,20 +1,50 @@
 #nullable enable
 using MagicOnion;
 using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 namespace MagicOnionLab.Unity
 {
     public static class ChannelFactory
     {
-        public static async ValueTask<GrpcChannelx> CreateAsync(string host, int tryCount = 3, int intervalSec = 3)
+        private static ConcurrentDictionary<string, GrpcChannelx> _channelHolder = new ConcurrentDictionary<string, GrpcChannelx>();
+
+        /// <summary>
+        /// Create GrpcChannelx or Get from cache. Please do not Dispose GrpcChannelx obtained from this factory.
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="tryCount"></param>
+        /// <param name="intervalSec"></param>
+        /// <returns></returns>
+        /// <exception cref="ApplicationException"></exception>
+        public static async ValueTask<GrpcChannelx> GetOrCreateAsync(string host, int tryCount = 3, int intervalSec = 3)
         {
+            if (_channelHolder.TryGetValue(host, out var cached))
+            {
+                return cached;
+            }
+
             for (var i = 0; i < tryCount; i++)
             {
                 try
                 {
                     var channel = GrpcChannelx.ForAddress(host);
-                    return channel;
+                    if (_channelHolder.TryAdd(host, channel))
+                    {
+                        return channel;
+                    }
+                    else
+                    {
+                        // duplicated, dispose it.
+                        await channel.DisposeAsync();
+
+                        // re-obtain already in-use channel
+                        if (_channelHolder.TryGetValue(host, out var cached2))
+                        {
+                            return cached2;
+                        }
+                    }
                 }
                 catch (Grpc.Core.RpcException)
                 {
@@ -27,6 +57,16 @@ namespace MagicOnionLab.Unity
                 }
             }
             throw new ApplicationException($"Cannot connect to the server. {host}");
+        }
+
+        public static async Task ClearAsync()
+        {
+            foreach (var channel in _channelHolder)
+            {
+                await channel.Value.DisposeAsync();
+            }
+
+            _channelHolder.Clear();
         }
     }
 }
